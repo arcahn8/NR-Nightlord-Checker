b161e0f406e3fc4ce2df2d17cd226da655beef8d
arcahn8 <arcahn8@gmail.com>
2026-05-30 15:05:04 +0900

    Update build.rs

--- build.rs
+++ build.rs
@@ -2,6 +2,13 @@ fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
         let mut res = winres::WindowsResource::new();
         res.set_icon("icon.ico");
+
+        res.set("FileDescription", "NightReign Save File Decrypter and Boss Reader");
+        res.set("ProductName", "Nightlord Checker");
+        res.set("OriginalFilename", "nightlord_checker.exe");
+        res.set("CompanyName", "arcahn8");
+        res.set("LegalCopyright", "Copyright © 2026");
+
         res.compile().unwrap();
     }
 }
